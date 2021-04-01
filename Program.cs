7aab580abc9ada62cf7c155374e43cf6cbfa980f
Memorik <70646403+Memorik@users.noreply.github.com>
2021-04-01 10:48:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class Program
+    {
+        private delegate double del(int a, int b, int c);
+        static void Main(string[] args)
+        {
+            N1();
+            N2();
+            n3();
+        }
+
+        static void N1()
+        {
+            del del = delegate (int a, int b, int c)
+            {
+                double result = (a + b + c) / 3;
+                return result;
+            };
+            Console.WriteLine(del(2, 3, 4));
+        }
+
+        static void N2()
+        {
+            Calc calc = new Calc();
+            Console.WriteLine(calc.Add(1, 3));
+            Console.WriteLine(calc.Sub(4, 2));
+            Console.WriteLine(calc.Div(12, 3));
+            Console.WriteLine(calc.Mul(5, 5));         
+        }
+
+        static void n3()
+        {
+            foreach (var del in N3.arr)
+            {
+                Console.WriteLine(del());
+            }
+        }
+    }
+}
---
+++ n3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_1
+{
+    public delegate int delegates();
+
+    public static class Ran
+    {
+        public static int random()
+        {
+            Random r = new Random();
+            return r.Next(50);
+        }
+    }
+    public class N3
+    {
+        public static delegates[]  arr = new delegates[]
+        {
+            new delegates(Ran.random),
+            new delegates(Ran.random),
+            new delegates(Ran.random)
+        };
+    }
+}
